4cede76cf7587c7cb0f5f922effb11adbe828c35
amandusw <amandusw@outlook.com>
2025-10-22 13:27:53 +0200

    s

--- visak.php
+++ visak.php
@@ -4,7 +4,7 @@
 */
   include("db.php");  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
 
-  $sqlSetning="SELECT FROM klasse;";
+  $sqlSetning="SELECT * FROM klasse;";
 
   $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
     /* SQL-setning sendt til database-serveren */
