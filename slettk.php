7aef722c9cea34d9688af5e2eec61f42adf69e09
amandusw <amandusw@outlook.com>
2025-10-22 13:53:06 +0200

    s

--- slettk.php
+++ slettk.php
@@ -1,34 +1,33 @@
-<?php  /* slett-klasser */
-/*
-/*  Programmet lager et skjema for å velge et poststed som skal slettes  
-/*  Programmet sletter det valgte poststedet
+<?php  
+/* slett-klasser
+   Programmet lar brukeren velge en klassekode og slette den fra databasen
 */
-?> 
+?>
 
-<script src="funksjoner.js"> </script>
+<script src="funksjoner.js"></script>
 
 <h3>Slett klasse</h3>
 
-<form method="post" action="" id="slettKlasseSkjema" name="slettKlasseSkjema" onSubmit="return bekreft()">
-  Klassekode 
+<form method="post" action="" id="slettKlasseSkjema" name="slettKlasseSkjema" onsubmit="return bekreft()">
+  Klassekode:
   <select name="klassekode" id="klassekode" required>
-    <option value=""> Velg klassekode </option>
+    <option value="">Velg klassekode</option>
+
     <?php
-      include("db.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
+      include("db.php");  // kobler til database
 
-      $sqlSetning="SELECT * FROM klasse;";
-      $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
-      $antallRader=mysqli_num_rows($sqlResultat); 
+      $sqlSetning = "SELECT * FROM klasse;";
+      $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å hente data fra databasen");
+      $antallRader = mysqli_num_rows($sqlResultat);
 
-      for ($r=1;$r<=$antallRader;$r++)
-        {
-          $rad=mysqli_fetch_array($sqlResultat);  
-          $klassekode=$rad["klassekode"];        
-
-          print ("<option value='$klassekode'> $klassekode </option>");
-        }
+      for ($r = 1; $r <= $antallRader; $r++) {
+        $rad = mysqli_fetch_array($sqlResultat);
+        $klassekode = $rad["klassekode"];
+        print("<option value='$klassekode'>$klassekode</option>");
+      }
     ?>
-    </select> <br><br/>
+  </select>
+  <br><br/>
 
   <input type="submit" value="Slett klasse" name="slettKlasseKnapp" id="slettKlasseKnapp" /> 
 </form>
@@ -36,34 +35,32 @@
 <?php
 mysqli_report(MYSQLI_REPORT_OFF);
 
-  if (isset($_POST ["slettKlasseKnapp"]))
-    {	
-      $klassekode=$_POST ["klassekode"];
-	  
-	  if (!$klassekode)
-        {
-          print ("Postnr m&aring; fylles ut");
-        }
-      else
-        {
-          include("db.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
+if (isset($_POST["slettKlasseKnapp"])) {	
+  $klassekode = $_POST["klassekode"];
+  
+  if (!$klassekode) {
+    print("Du må velge en klassekode.");
+  } else {
+    include("db.php");
 
-          $sqlSetning="SELECT * FROM klasse WHERE klasse='$klassekode';";
-          $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
-          $antallRader=mysqli_num_rows($sqlResultat); 
+    // Sjekk om klassen finnes
+    $sqlSetning = "SELECT * FROM klasse WHERE klassekode='$klassekode';";
+    $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å hente data fra databasen");
+    $antallRader = mysqli_num_rows($sqlResultat);
 
-          if ($antallRader==0)  /* poststedet er ikke registrert */
-            {
-              print ("Poststedet finnes ikke");
-            }
-          else
-            {	  
-              $sqlSetning="DELETE FROM klasse WHERE klassekode='$klassekode';";
-              mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
-                /* SQL-setning sendtttt til database-serveren */
-		
-              print ("F&oslash;lgende poststed er n&aring; slettet: $klassekode  <br />");
-            }
-        }
+    if ($antallRader == 0) {
+      print("Klassen finnes ikke i databasen.");
+    } else {
+      // Slett klassen
+      $sqlSetning = "DELETE FROM klasse WHERE klassekode='$klassekode';";
+      $ok = mysqli_query($db, $sqlSetning);
+
+      if ($ok) {
+        print("Følgende klasse er nå slettet: <b>$klassekode</b><br>");
+      } else {
+        print("Feil ved sletting: " . mysqli_error($db));
+      }
     }
-?> 
+  }
+}
+?>
