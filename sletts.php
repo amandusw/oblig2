fe65bdc2b5d8f6f126325003c682b6893ee70cfc
amandusw <amandusw@outlook.com>
2025-10-23 12:20:43 +0200

    s

---
+++ sletts.php
@@ -0,0 +1,70 @@
+<?php  
+/* slett-student
+   Programmet lar brukeren velge en student (brukernavn) og slette den fra databasen
+*/
+?>
+
+<script src="funksjoner.js"></script>
+
+<h3>Slett student</h3>
+
+<form method="post" action="" id="slettStudentSkjema" name="slettStudentSkjema" onsubmit="return bekreft()">
+  Brukernavn:
+  <select name="brukernavn" id="brukernavn" required>
+    <option value="">Velg student</option>
+
+    <?php
+      include("db.php");  // kobler til database
+
+      $sqlSetning = "SELECT * FROM student ORDER BY brukernavn;";
+      $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å hente data fra databasen");
+      $antallRader = mysqli_num_rows($sqlResultat);
+
+      for ($r = 1; $r <= $antallRader; $r++) {
+        $rad = mysqli_fetch_array($sqlResultat);
+        $brukernavn = $rad["brukernavn"];
+        $fornavn = $rad["fornavn"];
+        $etternavn = $rad["etternavn"];
+        $klassekode = $rad["klassekode"];
+
+        print("<option value='$brukernavn'>$brukernavn - $fornavn $etternavn ($klassekode)</option>");
+      }
+    ?>
+  </select>
+  <br><br/>
+
+  <input type="submit" value="Slett student" name="slettStudentKnapp" id="slettStudentKnapp" /> 
+</form>
+
+<?php
+mysqli_report(MYSQLI_REPORT_OFF);
+
+if (isset($_POST["slettStudentKnapp"])) {	
+  $brukernavn = $_POST["brukernavn"];
+  
+  if (!$brukernavn) {
+    print("Du må velge en student.");
+  } else {
+    include("db.php");
+
+    // Sjekk om studenten finnes
+    $sqlSetning = "SELECT * FROM student WHERE brukernavn='$brukernavn';";
+    $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å hente data fra databasen");
+    $antallRader = mysqli_num_rows($sqlResultat);
+
+    if ($antallRader == 0) {
+      print("Studenten finnes ikke i databasen.");
+    } else {
+      // Slett studenten
+      $sqlSetning = "DELETE FROM student WHERE brukernavn='$brukernavn';";
+      $ok = mysqli_query($db, $sqlSetning);
+
+      if ($ok) {
+        print("Følgende student er nå slettet: <b>$brukernavn</b><br>");
+      } else {
+        print("Feil ved sletting: " . mysqli_error($db));
+      }
+    }
+  }
+}
+?>
